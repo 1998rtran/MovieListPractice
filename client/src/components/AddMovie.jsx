2b21fc057d39e17424aea09b10234416496bf6bb
1998rtran <1998rtran@gmail.com>
2024-02-09 23:56:23 -0800

    created basic for add movie component

---
+++ client/src/components/AddMovie.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AddMovie() {
+  return (
+    <div>
+      <input placeholder='Add your favorite movie!' type='text'/>
+      <button>Add Movie!</button>
+    </div>
+  );
+}
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import AddMovie from './AddMovie';
 
 export default function App() {
   return (
     <div id="App">
+      <AddMovie />
       <p>Hello, world!</p>
     </div>
   );
