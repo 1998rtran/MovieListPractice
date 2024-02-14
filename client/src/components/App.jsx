ee590ca8b51682deebf36c8ae30e4c5de680f6a4
1998rtran <1998rtran@gmail.com>
2024-02-13 23:38:48 -0800

    created search function

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -11,7 +11,9 @@ export default function App() {
     {title: 'Spider-man', watched: false}
   ];
   const [movieList, setMovieList] = useState(list);
+  const [permList, setPermList] = useState(list);
   const [movieTitle, setMovieTitle] = useState('');
+  const [searchInput, setSearchInput] = useState('');
 
 const changeTitle = (e) => {
   setMovieTitle(e.target.value);
@@ -24,19 +26,24 @@ const handleAdd = () => {
   setMovieTitle('');
 }
 
-const searchList = () => {
-var searchedList = movieList;
-
+const handleSearch = (e) => {
+setSearchInput(e.target.value);
 }
 
-// useEffect(() => {
+var searchedList = movieList.filter((movie) => {
+  {return movie.title.toLowerCase().includes(searchInput.toLowerCase())}
+})
 
-// }, movieList)
+useEffect(() => {
+  if (searchInput !== 0) {
+    setMovieList(searchedList);
+  }
+}, [searchInput])
 
   return (
     <div id="App">
       <h1>Your Movie List!</h1>
-      <input type="text" placeholder="Search here!"/>
+      <input type="text" placeholder="Search here!" onChange={handleSearch} value={searchInput} />
       <AddMovie movieTitle={movieTitle} changeTitle={changeTitle} handleAdd={handleAdd}/>
       <MovieList movieList={movieList}/>
     </div>
